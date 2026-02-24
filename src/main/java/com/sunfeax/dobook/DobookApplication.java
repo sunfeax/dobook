75fc98ae731ea888925f500e96bd3e1e469e6c60
sunfeax <sunfeax@gmail.com>
2026-02-24 13:50:00 +0100

    first commit

---
+++ src/main/java/com/sunfeax/dobook/DobookApplication.java
@@ -0,0 +1,13 @@
+package com.sunfeax.dobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DobookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunfeax/dobook/DobookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunfeax.dobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
